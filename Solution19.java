413c5dd348aa6fe8f8f4e8e722ea65f880759a89
오어진 <dhdjwlsdlaehk123@naver.com>
2021-11-08 12:59:44 +0900

    모의고사

---
+++ Solution19.java
@@ -0,0 +1,41 @@
+//1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+//2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+//3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+//
+//1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+import java.util.*;
+
+class Solution19 {
+    public int[] solution(int[] answers) {
+
+        int[] person1 = {1, 2, 3, 4, 5};
+        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int[] cnt = new int[3];
+
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == person1[i%5]) cnt[0]++;
+            if(answers[i] == person2[i%8]) cnt[1]++;
+            if(answers[i] == person3[i%10]) cnt[2]++;
+        }
+
+        int max = Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
+
+        List<Integer> list = new ArrayList<>();
+
+        if(max == cnt[0])
+            list.add(1);
+        if(max == cnt[1])
+            list.add(2);
+        if(max == cnt[2])
+            list.add(3);
+
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
